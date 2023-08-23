d49d8aba057a307091958ef87a035dca70a9c831
Swapna V <vswapna3202@gmail.com>
2023-08-23 12:20:40 +0100

    feat: refactored further and removed hardcoded messages

--- src/main/java/com/techreturners/encapsulation/bankaccount/model/WeatherReporter.java
+++ src/main/java/com/techreturners/encapsulation/bankaccount/model/WeatherReporter.java
@@ -7,11 +7,15 @@ public class WeatherReporter {
     /*  Changing access of public to private for encapsulation */
     private String location;
     private double temperature;
+    /* Added static variables to hold hardcoded values */
     private static final String LONDON_CITY = "London";
     private static final String CALIFORNIA_CITY = "California";
     private static final String CAPE_TOWN_CITY = "Cape Town";
     private static final int HIGH_TEMPERATURE_THRESHOLD = 30;
     private static final int LOW_TEMPERATURE_THRESHOLD = 10;
+    private static final String HOT_MESSAGE = "It's too hot 🥵!";
+    private static final String COLD_MESSAGE = "It's too cold 🥶!";
+    private static final String COMFORTABLE_MESSAGE = "Ahhh...it's just right 😊!";
 
 
     public WeatherReporter(String location, double temperature) {
@@ -29,8 +33,13 @@ public class WeatherReporter {
     public String print() {
 
         double newTemp = convertTemperatureToFahrenheit(temperature);
-        return MessageFormat.format("I am in {0} and it is {1}. {2}. The temperature in Fahrenheit is {3}.", location, getWeatherEmoji(), getTemperatureFeedback(), newTemp);
-
+        return MessageFormat.format("I am in {0} and it is {1}. {2}. " +
+                "The temperature in Fahrenheit is {3}.",
+                location,
+                getWeatherEmoji(),
+                getTemperatureFeedback(),
+                newTemp
+                );
     }
 
     /* This method checks the location and sends back a weather emoji depending
@@ -57,14 +66,14 @@ public class WeatherReporter {
     public String getTemperatureFeedback() {
         if (temperature > HIGH_TEMPERATURE_THRESHOLD) {
 
-            return "It's too hot 🥵!";
+            return HOT_MESSAGE;
 
         } else if (temperature < LOW_TEMPERATURE_THRESHOLD) {
 
-            return "It's too cold 🥶!";
+            return COLD_MESSAGE;
 
         }
-        return "Ahhh...it's just right 😊!";
+        return COMFORTABLE_MESSAGE;
     }
 
 }
